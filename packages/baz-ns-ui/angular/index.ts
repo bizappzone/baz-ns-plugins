4f09c0f56c9916d06e7722f09c310a501a5374cb
aj <art@ampsire.com>
2021-08-23 14:59:06 -0400

    add angular support

---
+++ packages/baz-ns-ui/angular/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { BazNsUi } from '@bizappzone/baz-ns-ui';
+
+@NgModule()
+export class NativeScriptBazNsUiModule {}
+
+// Uncomment this line if the package provides a custom view component
+// registerElement('BazNsUi', () => BazNsUi);
